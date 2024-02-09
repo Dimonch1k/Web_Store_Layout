380980638fbf82258d9323de9a3bc9bdf8a4d692
Dimonch1k <137522769+Dimonch1k@users.noreply.github.com>
2024-02-09 14:42:29 +0200

    Add files via upload

---
+++ JS_Homework_09.02.2024/3_JS/2_Main/Catalog/Product_.js
@@ -0,0 +1,15 @@
+class Products_Catalog {
+	constructor() {
+
+	}
+
+	#initElements() {
+		this.PROD_IMG = document.querySelector('#prod_img')
+	}
+
+	#addListeners() {
+		this.PROD_IMG.addEventListener('', (e) => {
+			
+		})
+	}
+}
